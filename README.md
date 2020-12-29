1c580e0285e76e37fab17870f0ef766d085ea6af
Senthooran Rajamanoharan <s.rajamanoharan@gmail.com>
2020-12-29 21:30:41 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # config
+
+This repository is for storing various config files I commonly need across systems. Currently this consists of:
+
+* My emacs `init.el` script.
---
+++ init.el
@@ -0,0 +1,96 @@
+;; .emacs.d/init.el
+;; Initially based on https://realpython.com/emacs-the-best-python-editor/
+
+;; ===================================
+;; MELPA Package Support
+;; ===================================
+;; Enables basic packaging support
+(require 'package)
+
+;; Adds the Melpa archive to the list of available repositories
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+;; Initializes the package infrastructure
+(package-initialize)
+
+;; If there are no archived package contents, refresh them
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Installs packages
+;;
+;; myPackages contains a list of package names
+(defvar myPackages
+  '(better-defaults                 ;; Set up some better Emacs defaults
+    material-theme                  ;; Theme
+    elpy                            ;; Emacs Lisp Python Environment
+    flycheck                        ;; Syntax checking
+    yaml-mode                       ;; For editing yaml
+    markdown-mode                   ;; Editing md documents
+    )
+  )
+
+;; Scans the list in myPackages
+;; If the package listed is not already installed, install it
+(mapc #'(lambda (package)
+          (unless (package-installed-p package)
+            (package-install package)))
+      myPackages)
+
+;; ===================================
+;; Basic Customization
+;; ===================================
+
+(setq inhibit-startup-message t)    ;; Hide the startup message
+(load-theme 'material t)            ;; Load material theme
+(global-linum-mode t)               ;; Enable line numbers globally
+(setq column-number-mode t)         ;; Show column and line numbers
+
+;; ====================================
+;; Development Setup
+;; ====================================
+;; Enable elpy
+(elpy-enable)
+
+;; Enable Flycheck
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; Enable yaml-mode
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+(add-hook 'yaml-mode-hook
+          '(lambda ()
+             (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
+;; Markdown mode
+(setq markdown-command "pandoc")
+
+
+;; ====================================
+;; TeX setup
+;; ====================================
+;;
+;; Enable aspell for spell checking. Note the commented out line is only
+;; required in Windows.
+;; (add-to-list 'exec-path "C:/Program Files/Aspell/bin/")
+(setq-default ispell-program-name "aspell"
+	      ispell-personal-dictionary ".ispell")
+(require 'ispell)
+
+
+
+;; User-Defined init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (elpy material-theme better-defaults markdown-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
